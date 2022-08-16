b6bef981d4444cf48fdfa9558cd92874dccf46ff
HitanshuShah1001 <hitanshushah5@gmail.com>
2022-08-16 14:31:32 +0530

    heroku deployement

--- auth.js
+++ auth.js
@@ -4,9 +4,6 @@ const jwt = require('jsonwebtoken');
 module.exports = async(req,res,next) => {
     try {
         const token = await req.headers.authorization.split(" ")[1]
-        console.log(req.headers.authorizations)
-
-        console.log(token,'token');
 
         const decodedtoken  = await jwt.verify(token,"secret")
 
@@ -16,7 +13,7 @@ module.exports = async(req,res,next) => {
 
         next();
 
-        
+
 
     }catch(error){
         res.status(401).json({
